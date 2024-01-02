f39fb9055fa2b244fc0f61392b983da20f850646
jhsong76 <20190976@sungshin.ac.kr>
2024-01-02 20:28:32 +0900

    feat: 스프링부트 버전 변경

--- src/main/java/com/hackerton/demo/controller.java
+++ src/main/java/com/hackerton/demo/controller.java
@@ -1,4 +1,4 @@
-package com.hackerton.demo.controller;
+package com.hackerton.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
