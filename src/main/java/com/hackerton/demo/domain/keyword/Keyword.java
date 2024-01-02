a4bcb85afb9c01cb3a8716191b2da1f74ac280ad
zzimnii <happykeil@naver.com>
2024-01-03 03:00:48 +0900

    feat: 키워드 관련 장소 추천 API

---
+++ src/main/java/com/hackerton/demo/domain/keyword/Keyword.java
@@ -0,0 +1,21 @@
+package com.hackerton.demo.domain.keyword;
+
+import com.hackerton.demo.domain.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@AllArgsConstructor
+public class Keyword extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "keyword_id")
+    private Long id;
+
+    private String optionTitle;
+}
