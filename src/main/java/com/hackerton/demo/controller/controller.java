070a0811338b779f3cdbe3d6f41adc1691ce39ef
jhsong76 <20190976@sungshin.ac.kr>
2024-01-02 16:45:06 +0900

    fix: /healthy 테스트 추가 #1

---
+++ src/main/java/com/hackerton/demo/controller/controller.java
@@ -0,0 +1,13 @@
+package com.hackerton.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+    @GetMapping("/healthy")
+    public String healthCheck() {
+        return "I'm healthy!";
+    }
+
+}
