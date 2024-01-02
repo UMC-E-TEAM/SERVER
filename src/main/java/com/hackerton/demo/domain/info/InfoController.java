97104c4e891f43a1bffb328b614f169dc512f9a1
jhsong76 <20190976@sungshin.ac.kr>
2024-01-02 21:58:25 +0900

    feat: swagger 설정 #10

--- src/main/java/com/hackerton/demo/domain/info/InfoController.java
+++ src/main/java/com/hackerton/demo/domain/info/InfoController.java
@@ -2,6 +2,8 @@ package com.hackerton.demo.domain.info;
 
 import com.hackerton.demo.domain.common.ApiResponse;
 import com.hackerton.demo.domain.common.ApiResponseStatus;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class InfoController {
     private final InfoService infoService;
 
-    @GetMapping("/")
+    @GetMapping("/getHome")
+    @Operation(summary = "홈 화면 오늘의 알쓸신잡 조회 API", description = "홈 화면에 띄워지는 알쓸신잡 정보를 조회합니다.")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "GET_SUCCESS", description = "조회 성공"),
+    })
     public ResponseEntity<ApiResponse<InfoDto>> getHome() {
         ApiResponse<InfoDto> response = new ApiResponse<>(ApiResponseStatus.GET_SUCCESS, infoService.getHome());
         return ResponseEntity.ok(response);
