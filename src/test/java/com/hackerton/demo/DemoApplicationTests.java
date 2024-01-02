610677ddd10946ce6577d2b49515c4d6a8014c82
unknown <yjm1800@naver.com>
2024-01-02 15:52:19 +0900

    first commit

---
+++ src/test/java/com/hackerton/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackerton.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
