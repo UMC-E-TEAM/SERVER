7cb6df687e61ab830d07ac5806e6050acd1e0f79
jhsong76 <20190976@sungshin.ac.kr>
2024-01-02 22:34:53 +0900

    feat: ci/cd 파이프라인 구축

--- src/main/java/com/hackerton/demo/controller.java
+++ src/main/java/com/hackerton/demo/controller.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class controller {
-    @GetMapping("/healthy")
+    @GetMapping("/health")
     public String healthCheck() {
         return "I'm healthy!";
     }
